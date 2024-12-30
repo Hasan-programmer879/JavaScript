f7df7fa737f58319f8a8b18a0e14fe423d1aa6ae
Hasan-programmer879 <alihassanmansoori@gmail.com>
2024-12-30 17:18:53 +0000

    data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict "; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+
+console.log(3
+    +
+    3) //code readibility should be high
+
+console.log("Hasan")
+
+let name="hasan"
+let age="18"
+let isloggedin = false
+
+//number => 2 to power 53
+// bigInt //normally not use in smaller projects big companies use this like facebook
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => to find uniqueness
+
+// object
+
+console.log(typeof undefined)//undefined
+
+console.log(typeof null)//object
