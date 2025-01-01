8fa814ecb4cd2118c380bc48f77a6f5921c99e28
Hasan-programmer879 <alihassanmansoori@gmail.com>
2025-01-01 11:51:08 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+//let score = "hasan";
+
+//console.log(typeof (score))
+//console.log(typeof score)
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1;false=>0
+
+//let IsLoggedIn= 1
+//let BooleanIsLoggedIn =Boolean(IsLoggedIn)
+//console.log(BooleanIsLoggedIn)
+
+// 1=>true ;0=>false
+// ""=>false
+// "hasan"=>true
+
+let anyNumber =33
+
+let stringNumber =String(anyNumber)
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber);
